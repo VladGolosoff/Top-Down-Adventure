44685db78873a596614a46b27dd6f5a20a0927a8
Vladislav Golosov <v.golosoff@gmail.com>
2024-09-19 16:23:17 +0300

    add input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,42 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
+    
     [SerializeField] private float movingSpeed = 5f;
+    
 
     private void Awake()
     {
+        
         _rigidbody = GetComponent<Rigidbody2D>();
+        
     }
 
+   
     private void FixedUpdate()
     {
-        Vector2 inputVector = new Vector2(0, 0);
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
         
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1f;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1f;
-        }
-        
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1f;
-        }
-        
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1f;
-        }
-
+ 
         inputVector = inputVector.normalized;
         
         _rigidbody.MovePosition(_rigidbody.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
