39f7ab293b92d51aa48af3e9926e0a2a5bf56485
Vladislav Golosov <v.golosoff@gmail.com>
2024-09-26 12:28:04 +0300

    add animation logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,14 +7,17 @@ using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+
     private Rigidbody2D _rigidbody;
-    
+    private float minMovingSpeed = 0.1f;
+    private bool isRunning = false;
     [SerializeField] private float movingSpeed = 5f;
     
 
     private void Awake()
     {
-        
+        Instance = this;
         _rigidbody = GetComponent<Rigidbody2D>();
         
     }
@@ -22,11 +25,27 @@ public class Player : MonoBehaviour
    
     private void FixedUpdate()
     {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
         Vector2 inputVector = GameInput.Instance.GetMovementVector();
-        
- 
         inputVector = inputVector.normalized;
-        
         _rigidbody.MovePosition(_rigidbody.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
+
+        if (Mathf.Abs(inputVector.x) > minMovingSpeed || Mathf.Abs(inputVector.y) > minMovingSpeed)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+    }
+
+    public bool IsRunning()
+    {
+        return isRunning;
     }
 }
